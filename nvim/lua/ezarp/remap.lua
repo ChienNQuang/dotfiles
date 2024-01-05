524519612520b321f901b678189b6ff43539a05e
Chien Nguyen Quang <quangchieens1@gmail.com>
2024-01-06 00:38:56 +0700

    add: some remaps

--- nvim/lua/ezarp/remap.lua
+++ nvim/lua/ezarp/remap.lua
@@ -1,3 +1,17 @@
-
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- window resize
+vim.keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]]) -- make the window biger vertically
+vim.keymap.set("n", "-", [[<cmd>vertical resize -5<cr>]]) -- make the window smaller vertically
+vim.keymap.set("n", "+", [[<cmd>horizontal resize +2<cr>]]) -- make the window bigger horizontally by pressing shift and =
+vim.keymap.set("n", "_", [[<cmd>horizontal resize -2<cr>]]) -- make the window smaller horizontally by pressing shift and -
+
+-- window jumping
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+
+vim.keymap.set("n", "<A-t>", "<C-w><C-V>")
+vim.keymap.set("n", "<A-w>", "<C-w><C-c>")
