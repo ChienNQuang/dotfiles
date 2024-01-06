79bf09263eac8152baeb935ea593793b6a050221
Chien Nguyen Quang <quangchieens1@gmail.com>
2024-01-07 04:16:51 +0700

    add: some remaps and default settings

--- nvim/lua/ezarp/init.lua
+++ nvim/lua/ezarp/init.lua
@@ -1,2 +1,7 @@
 require("ezarp.remap")
 require("ezarp.packer")
+
+vim.cmd.set("relativenumber")
+vim.cmd.set("tabstop=4")
+vim.cmd.set("softtabstop=0 noexpandtab")
+vim.cmd.set("shiftwidth=4")
--- nvim/lua/ezarp/remap.lua
+++ nvim/lua/ezarp/remap.lua
@@ -2,10 +2,16 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- window resize
-vim.keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]]) -- make the window biger vertically
-vim.keymap.set("n", "-", [[<cmd>vertical resize -5<cr>]]) -- make the window smaller vertically
-vim.keymap.set("n", "+", [[<cmd>horizontal resize +2<cr>]]) -- make the window bigger horizontally by pressing shift and =
-vim.keymap.set("n", "_", [[<cmd>horizontal resize -2<cr>]]) -- make the window smaller horizontally by pressing shift and -
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>")
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>")
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>")
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
+
+-- terminal
+vim.keymap.set("t", "<C-Up>", "<cmd>resize -2<CR>")
+vim.keymap.set("t", "<C-Down>", "<cmd>resize +2<CR>")
+vim.keymap.set("t", "<C-Left>", "<cmd>vertical resize -2<CR>")
+vim.keymap.set("t", "<C-Right>", "<cmd>vertical resize +2<CR>")
 
 -- window jumping
 vim.keymap.set("n", "<C-h>", "<C-w>h")
@@ -15,3 +21,19 @@ vim.keymap.set("n", "<C-k>", "<C-w>k")
 
 vim.keymap.set("n", "<A-t>", "<C-w><C-V>")
 vim.keymap.set("n", "<A-w>", "<C-w><C-c>")
+
+-- terminal
+vim.keymap.set("t", "<C-h>", "<cmd>wincmd h<CR>")
+vim.keymap.set("t", "<C-j>", "<cmd>wincmd j<CR>")
+vim.keymap.set("t", "<C-k>", "<cmd>wincmd k<CR>")
+vim.keymap.set("t", "<C-l>", "<cmd>wincmd l<CR>")
+
+-- move block tab wise
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+vim.keymap.set("n", "<TAB>", ":bn<CR>")
+vim.keymap.set("n", "<S-TAB>", ":bp<CR>")
+vim.keymap.set("n", "<leader>bd", ":bd<CR>") -- from Doom Emacs
+
+vim.keymap.set("n", "<F2>", ":lua vim.lsp.buf.rename()<CR>")
